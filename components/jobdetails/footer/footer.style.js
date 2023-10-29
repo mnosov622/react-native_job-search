229a3d6b684918cc5c3022193b20a57c21a3483a
Maxim N <mnosov622@gmail.com>
2023-10-28 23:35:30 -0400

    added apply now functionality

--- components/jobdetails/footer/footer.style.js
+++ components/jobdetails/footer/footer.style.js
@@ -36,6 +36,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginLeft: SIZES.medium,
     borderRadius: SIZES.medium,
+    padding: 15,
   },
   applyBtnText: {
     fontSize: SIZES.medium,
